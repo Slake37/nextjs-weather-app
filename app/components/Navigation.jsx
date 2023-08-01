dd8d11695db6d951cbb428e90c18f6fa4f82d5ed
Slake37 <moldovan.alin160@yahoo.co.uk>
2023-08-01 21:01:03 +0100

    lol2

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -7,7 +7,7 @@ export const robotoCondesed = Roboto_Condensed({
 
 const Navigation = () => {
   return (
-    <div className="fixed w-full top-0 bg-white  z-10 text-[#222831]">
+    <div className="fixed top-0 bg-white w-full  z-10 text-[#222831]">
         <div className="flex justify-start items-center text-[#222831] p-5 bg-transparent">
             <BsFillCloudSunFill className="text-xl md:text-2xl"/>
             <h1 className='text-xl font-bold mx-2 md:text-2xl'>Weather web app</h1>
--- app/components/threedaysforecast.jsx
+++ app/components/threedaysforecast.jsx
@@ -16,20 +16,20 @@ const ThreedDaysForecast =async ({location}) => {
   return (
     <div className="flex flex-col justify-center items-center mt-5  py-5">
       <h1 className="text-2xl underline">Next 3 days forecast</h1>
-      <div className="flex justify-between items-center w-full mt-5">
+      <div className="flex justify-between items-center mt-5">
         {threeDaysForecast.forecast.forecastday.map((data) => (
-          <div className='flex flex-col justify-center items-center mx-2 border-2 rounded-md drop-shadow-2xl p-3'>
+          <div className='flex flex-col justify-center mx-1 items-center border-2 rounded-md drop-shadow-2xl p-3'>
             <p>{data.date}</p>
-            <img src={data.day.condition.icon} alt="" className='w-full'/>
+            <img src={data.day.condition.icon} alt="" className='w-10'/>
             <div className='flex justify-center items-center text-center'>
-              <p className='flex justify-center items-center text-center text-sm'><BsThermometerLow/>{data.day.mintemp_c}<RiCelsiusLine/></p>
+              <p className='flex justify-center items-center text-center text-xs'><BsThermometerLow/>{data.day.mintemp_c}<RiCelsiusLine/></p>
               <p>-</p>
-              <p className='flex justify-center items-center text-center text-sm'><BsThermometerHigh/>{data.day.maxtemp_c}<RiCelsiusLine/></p>
+              <p className='flex justify-center items-center text-center text-xs'><BsThermometerHigh/>{data.day.maxtemp_c}<RiCelsiusLine/></p>
             </div>
             <div className='flex justify-center items-center mt-3'>
-              <p className='flex flex-col justify-center items-center text-center text-sm'><BsFillSunriseFill className='text-2xl'/>{data.astro.sunrise}</p>
+              <p className='flex flex-col justify-center items-center text-center text-xs'><BsFillSunriseFill className='text-2xl'/>{data.astro.sunrise}</p>
               <p className='mx-1'>-</p>
-              <p className='flex flex-col justify-center items-center text-center text-sm'><BsFillSunsetFill className='text-2xl'/>{data.astro.sunset}</p>
+              <p className='flex flex-col justify-center items-center text-center text-xs'><BsFillSunsetFill className='text-2xl'/>{data.astro.sunset}</p>
             </div>
           </div>
         ))}
