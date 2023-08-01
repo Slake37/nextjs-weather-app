eb9421bbf294a56bdd6c2f0e1a55a34a45a33a67
Slake37 <moldovan.alin160@yahoo.co.uk>
2023-08-01 20:41:26 +0100

    second commit

--- app/page.js
+++ app/page.js
@@ -18,7 +18,7 @@ const HomePage = () => {
     <div className="h-screen flex flex-col justify-center items-center max-w-[720px]">
       <form className="flex flex-col justify-center items-start w-full">
         <label className="text-lg font-semibold">Please enter location</label>
-        <input type="text" name="" id="" placeholder="Please enter location" className="w-full outline-none p-1 rounded-md" onChange={(e) => setLocationSearched(e.target.value)}/>
+        <input type="text"  placeholder="Please enter location" className="w-full outline-none p-1 rounded-md" onChange={(e) => setLocationSearched(e.target.value)}/>
         <button onClick={handleSubmit} className="flex justify-center items-center mt-5 bg-[#222831] text-[#EEEEEE] w-full rounded-md drop-shadow-lg text-xl px-5 py-1 "><BsSearch className="mx-2"/> Search</button>
       </form>
     </div>
